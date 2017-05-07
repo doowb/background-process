4fff4ce54af16c5cf24eb2add90c7873c7218090
Rob Gleeson <robbie.gleeson@gmai.com>
2017-05-07 13:51:28 +0100

    Returned child process ID from background.start. Added unit tests.

--- test/test.js
+++ test/test.js
@@ -15,6 +15,21 @@ describe('background-process', function() {
   beforeEach(mkdirp.bind(mkdirp, actual()));
   afterEach(del.bind(del, actual()));
 
+  it('should return the child process id', function() {
+    this.timeout(10000);
+    var stdout = fs.openSync(actual('stdout.txt'), 'a');
+    var stderr = fs.openSync(actual('stderr.txt'), 'a');
+
+    // start the worker and pass options
+    const childPid = background.start(fixtures('worker.js'), {
+        stdio: [stdout, stderr]
+    });
+
+    if (typeof(childPid) !== Number || !childPid) {
+        console.log('Child ');
+    }
+  });
+
   it('should export an object', function() {
     assert(background);
     assert.equal(typeof background, 'object');
